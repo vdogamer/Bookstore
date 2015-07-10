b456a12d6d47fc3e1a3178849fb38beb102ee4a4
Cameron Diggs <ctd8z@virginia.edu>
2015-07-10 17:08:47 -0400

    Creating bootstrap styling to our views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,28 @@
 class BooksController < ApplicationController
   def new
+    # This method handles displaying the form for the books
   end
 
   def create
+    # This method handles the process of creating a new book from the form.
   end
 
   def update
+    # This method handles the processing of the edit requests.
   end
 
   def edit
+    # This method handles the actual form from the new method.
   end
 
   def destroy
+    # This method is used to delete book objects.
   end
 
   def index
   end
 
   def show
+    # This method displays one book-listing at a time.
   end
 end
